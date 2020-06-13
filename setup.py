d85f5b16ecc0589e84d4f2e480413a289062bab8
sparthaje <35978578+sparthaje@users.noreply.github.com>
2020-06-13 03:03:12 -0400

    bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.rst", "r") as f:
 
 setup(
     name="reverb_serial",
-    version="1.1.4",
+    version="1.2.1",
     license="GPL",
     packages=find_packages(),
     author="tjreverb",

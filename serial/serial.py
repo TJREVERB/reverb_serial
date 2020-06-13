8da93fab1523e1457ebf966071b1135fce770244
sparthaje <35978578+sparthaje@users.noreply.github.com>
2020-06-13 02:25:16 -0400

    Implemented reverb_serial

--- serial/serial.py
+++ serial/serial.py
@@ -1,48 +1,99 @@
 import os
-import json
+from time import time
 
-I2C_SMBUS_BLOCK_MAX = 32
+try:
+    import portalocker
+except:
+    raise ImportError("Run `pip install 'portalocker~=1.7.0'` in the correct virtual environment")
 
 PROJECT_ROOT = os.path.abspath(os.getcwd())
-#if "pfs" not in PROJECT_ROOT:
-#    raise RuntimeError("This package must be run within the TJREVERB pFS directory!")
-#while not PROJECT_ROOT.endswith("pfs"):
-#    PROJECT_ROOT = os.path.dirname(PROJECT_ROOT)
-PROJECT_ROOT = os.path.dirname(PROJECT_ROOT)
+if "pfs" not in PROJECT_ROOT:
+    print("\033[1;31mThis package must be run within the TJREVERB pFS directory!\033[0;0m")
+    print("\033[1;33mAssuming this is being run as a test of the package, output to folder `pfs-output`\033[0;0m")
+    PROJECT_ROOT = os.path.join(PROJECT_ROOT, "pfs-output")
+else:
+    while not PROJECT_ROOT.endswith("pfs"):
+        PROJECT_ROOT = os.path.dirname(PROJECT_ROOT)
+    PROJECT_ROOT = os.path.join(PROJECT_ROOT, "pfs-output")
+
+
+class SerialException(Exception):
+    pass
 
 
 class Serial:
 
-	def __init__(self, port=None, baudrate=9600, timeout=None):
-		if port == None:
-			raise RuntimeError("Port not specified")
-		self.port = port
-		self.baudrate = baudrate
-		self.timeout = timeout
-		self.open(port)
+    def __init__(self, port=None, buadrate=9600, timeout=float('inf')):
+        self.port = port
+        self.baudrate = buadrate
+        self.timeout = timeout
 
-	def open(self, port):
-		self.path = os.path.join(PROJECT_ROOT, port)
+        self.read_filename = None
+        self.write_filename = None
 
-	def write(self, message):
-		try:
-			with open(os.path.join(self.path, "write"), "r") as r:
-				queue = json.load(r)
-		except: queue = []
+        if not os.path.exists(PROJECT_ROOT):
+            os.makedirs(PROJECT_ROOT)
 
-		with open(os.path.join(self.path, "write"), "w") as w:
-			json.dump(queue+[message], w)
+        if self.port:
+            self.open()
 
-	def read(self, size=1):
-		try:
-			with open(os.path.join(self.path, "read"), "r") as r:
-				queue = json.load(r)
-		except: queue = []
+    @property
+    def is_open(self):
+        return not (self.read_filename is None and self.write_filename is None)
 
-		message = "\n".join(queue)
+    def open(self):
+        if self.port is None:
+            raise SerialException('Port must be configured before it can be used.')
 
-		with open(os.path.join(self.path, "read"), "w") as w:
-                        json.dump(message[size:].split("\n"), w)
+        self.read_filename = os.path.join(PROJECT_ROOT, self.port + '_pfs_rx.serial')
+        self.write_filename = os.path.join(PROJECT_ROOT, self.port + '_pfs_tx.serial')
 
-		return message[:size].encode("utf-8")
+    def close(self):
+        if self.is_open:
+            self.read_filename = None
+            self.write_filename = None
 
+    def flushInput(self):
+        if not self.is_open:
+            raise SerialException("Attempting to use a port that is not open")
+
+        with portalocker.Lock(self.read_filename, 'w') as rx:
+            pass
+
+    def flushOutput(self):
+        if not self.is_open:
+            raise SerialException("Attempting to use a port that is not open")
+
+        with portalocker.Lock(self.write_filename, 'w') as tx:
+            pass
+
+    def write(self, message):
+        if not self.is_open:
+            raise SerialException("Attempting to use a port that is not open")
+
+        if type(message) != bytes:
+            raise SerialException("Send the serial port bytes")
+
+        with portalocker.Lock(self.write_filename, 'a') as tx:
+            tx.write(message.decode('utf-8'))
+
+    def read(self, size=1):
+        if not self.is_open:
+            raise SerialException("Attempting to use a port that is not open")
+
+        if size > 1:
+            print("\033[1;33mOnly one byte/call with reverb-serial\033[0;0m")
+
+        start = time()
+        while time() - start < self.timeout:
+            rx_content = None
+            with portalocker.Lock(self.read_filename, 'r', timeout=self.timeout) as rx:
+                rx_content = rx.read()
+
+            if rx_content:
+                nb = rx_content[0].encode('utf-8')
+                with portalocker.Lock(self.read_filename, 'w') as rx:
+                    rx.write(rx_content[1:])
+                return nb
+
+        return b''
--- test.py
+++
@@ -1,5 +0,0 @@
-from serial import Serial
-
-s = Serial(port = "test")
-s.write("hello friends")
-print(s.read())
